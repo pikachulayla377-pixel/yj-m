dbfce96e858277333793ae824cc1e4b571ed7107
tushar-kant <tusharkantanayak713@gmail.com>
2026-01-12 17:41:06 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
               <SocialFloat />
 
       </body>
-       {/* <GoogleAnalytics gaId="G-XS42JM8W7L" /> */}
+       <GoogleAnalytics gaId="G-7MQ5K05HZQ" />
        {/* <script src="https://quge5.com/88/tag.min.js" data-zone="191906" async data-cfasync="false"></script> */}
     </html>
   );
